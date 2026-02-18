3f5b65103523a8790f6c195293abc2aa50d93048
SitesProjects229 <netlyfuu@icloud.com>
2026-02-18 21:29:57 +0000

    hgkl;

---
+++ _next/static/0MU8tJ1Ya7LusvyN_WIBc/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Farticles","\u002Farticles\u002F[slug]","\u002Fbonuses","\u002Fcalculator","\u002Fcalculators","\u002Fcalculators\u002F[slug]","\u002Ffaq","\u002Flogin","\u002Fnews","\u002Fnews\u002F[slug]","\u002Freviews","\u002Freviews\u002F[slug]","\u002Fvideos","\u002Fvideos\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
